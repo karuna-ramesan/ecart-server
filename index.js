899c25793f41805cd226a2e62b7da362bc2d11c2
karuna-ramesan <karunaramesh1213@gmail.com>
2023-05-03 13:42:36 +0530

    dynamic port setup

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const router=require('./routes/router')
 // create server app
 const server=express()
 // to store port number
-const PORT = 3000
+const PORT = 3000 || process.env.PORT
 
 
 // use cors express.json and router in server app
